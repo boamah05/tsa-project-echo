f9f3fe743afd2c3d520ecf7469524e498f297548
Hamzah <hamzahraoof@gmail.com>
2026-01-25 20:08:50 -0600

    emotion recognition

---
+++ cvstuff.py
@@ -0,0 +1,39 @@
+import cv2
+from deepface import DeepFace
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+capture = cv2.VideoCapture(0)
+
+if not capture.isOpened():
+    raise Exception("Could Not Start Video Capture")
+
+while True:
+    ret, frame = capture.read()
+    
+    if not ret:
+        raise Exception("Could Not Return Frame")
+    
+
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
+    faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30,30))
+
+    for (x,y,w,h) in faces:
+        face_roi = frame[y:y+h, x:x+w]
+        result = DeepFace.analyze(face_roi, actions = ['emotion'], enforce_detection = False)
+
+        emotion = result[0]["dominant_emotion"]
+
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 2)
+        cv2.putText(frame, emotion, (x,y-10), cv2.FONT_HERSHEY_SIMPLEX,0.9, (0,0,255),2)
+
+    cv2.imshow("Emotion Prediction", frame)
+
+    # Close From Loop When Pressed q
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
